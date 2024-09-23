13695aaa7ef0b881fac874ef948829628e6ccaa9
Viswajith K S <72287622+PsycoZombie@users.noreply.github.com>
2024-09-23 18:12:13 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,49 @@
+<?php
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$username=$_POST['username'];
+$password=$_POST['password'];
+echo"<br>";
+$con=new mysqli("localhost","viswajithks_20222108","20222108","animals","3306","");
+$query="select * from auth where username=\"".$username."\";";
+$doquerry=mysqli_query($con,$query);
+if(mysqli_num_rows($doquerry)==0)
+die("user not registered");
+while ($row=mysqli_fetch_array($doquerry)) {
+    if(hash('sha512',$password)==$row['password']){
+	$qy="select sid from auth where username=\"".$username."\";";
+	$doqy=mysqli_query($con,$qy);
+	$array=mysqli_fetch_array($doqy);
+	$sid=$array['sid'];
+	$qy="select * from studentslist where sid=\"".$sid."\";";
+	$doqy=mysqli_query($con,$qy);
+	echo ('<HTML>
+		<head>	
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+		</head>
+		<BODY class="bg-info">');
+	$array=mysqli_fetch_array($doqy);
+		echo'<p class="h1 text-center text-primary pt-5 pb-5" id="headingid"><strong>Student Details</strong></p>';
+		echo "<div class='container'><table class='table table-bordered table-dark table-striped'>";
+		echo"<tr><th>sid</th><td>".$array['sid']."</td></tr>";
+		echo"<tr><th>regno</th><td>".$array['regno']."</td></tr>";
+		echo"<tr><th>name</th><td>".$array['name']."</td></tr>";
+		echo"<tr><th>semester</th><td>".$array['semester']."</td></tr>";
+		echo"<tr><th>course</th><td>".$array['course']."</td></tr>";
+		echo"<tr><th>year of admission</th><td>".$array['adm_year']."</td></tr>";
+		echo"<tr><th>date of birth</th><td>".$array['dob']."</td></tr>";
+		echo"<tr><th>address</th><td>".$array['address']."</td></tr>";
+		echo"<tr><th>district</th><td>".$array['district']."</td></tr>";
+		echo"<tr><th>state</th><td>".$array['state']."</td></tr>";
+		echo"<tr><th>country</th><td>".$array['country']."</td></tr>";
+		echo"<tr><th>pincode</th><td>".$array['pincode']."</td></tr>";
+		echo"</table>";
+	$con->close();
+	echo('<p> New registration? <a href="newreg.html">Sign up here</a>.</p>');
+	echo('</div></BODY></HTML>');
+	die("");
+    }
+}
+$con->close();
+    die("wrong password");
+?>
---
+++ newreg.php
@@ -0,0 +1,64 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$host="localhost";
+$port=3306;
+$socket="";
+$user="viswajithks_20222108";
+$password="20222108";
+$dbname="animals";
+$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
+	or die ('Could not connect to the database server' . mysqli_connect_error());
+$regno=htmlspecialchars($_POST['regno']);
+$name=htmlspecialchars($_POST['name']);
+$semester=htmlspecialchars($_POST['semester']);
+$course=htmlspecialchars($_POST['course']);
+$adm_year=htmlspecialchars($_POST['adm_year']);
+$dob=htmlspecialchars($_POST['dob']);
+$address=htmlspecialchars($_POST['address']);
+$district=htmlspecialchars($_POST['district']);
+$state=htmlspecialchars($_POST['state']);
+$country=htmlspecialchars($_POST['country']);
+$pincode=htmlspecialchars($_POST['pincode']);
+$loginqr = "INSERT INTO studentslist (regno, name, semester, course, adm_year, dob, address, district, state, country, pincode) VALUES (" . $regno . ", \"" . $name . "\", \"" . $semester . "\", \"" . $course . "\", " . $adm_year . ", \"" . $dob . "\", \"" . $address . "\", \"" . $district . "\", \"" . $state . "\", \"" . $country . "\", " . $pincode . ");";
+$statement = $con->prepare($loginqr);
+try {
+    if(!$statement->execute())
+	;
+} catch (mysqli_sql_exception $e) { //for register number check
+    if($con->errno == 1062){
+        $con->close();
+        die("user already exists. please login");
+    }
+}
+$con->close();
+$username=htmlspecialchars($_POST['username']);
+$password=htmlspecialchars($_POST['password']);
+$con=new mysqli("localhost","viswajithks_20222108","20222108","animals","3306","");
+$query="select sid from studentslist where regno=".$regno.";";
+$sid="";
+$doquerry=mysqli_query($con,$query);
+while($final=mysqli_fetch_array($doquerry)){
+    $sid=$final['sid'];
+    $insert = "INSERT INTO auth (sid, username, password) VALUES (" . $sid . ",\"" . $username  . "\", \"" . hash('sha512',$password) . "\");";
+    $stat=$con->prepare($insert);
+	try {
+    if(!$stat->execute())
+	 $con->close();
+    echo("Registration Successfull");
+    echo('<p>Please <a href="login.html">login</a>.</p>');
+    die("");
+} catch (mysqli_sql_exception $e) { //for username check
+    if($con->errno == 1062){
+        $dltqry="delete from studentslist where regno=".$regno.";";
+        $dltstat=$con->prepare($dltqry);
+        $dltstat->execute();
+        $con->close();
+        die("username not available");
+    }
+}
+    break;
+}
+$con->close();
+?>
